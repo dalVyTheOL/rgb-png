a4184f4e22c7c50822a7875ba76b0542f980a364
Vladislav <levgdert@gmail.com>
2025-04-17 23:19:48 +0500

    Update rgb_input.py

--- rgb_input.py
+++ rgb_input.py
@@ -1,6 +1,6 @@
 from stegano import lsb
 
-secret = lsb.hide("input.png", "Steganography is cool")  # input.png это исходное сообщение, потом сообщение для скрытия
+secret = lsb.hide("input.png", "Steganography is cool")  # input.png это исходное изображение, потом сообщение для скрытия
 secret.save("output.png")  # Новый файл со скрытой информацией
 
 
